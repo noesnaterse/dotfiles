06224149a657424f75feee31fe4d866454ce52a9
Noe <noe@raspberrypi.(none)>
2013-04-09 08:33:49 +0000

    Added a tmux.conf

--- install.sh
+++ install.sh
@@ -11,10 +11,12 @@ rm ~/.vimrc
 rm -rf ~/.vim
 rm ~/.bashrc
 rm ~/.profile
+rm ~/.tmux.conf
 
 ln -s $destination/vimrc ~/.vimrc
 ln -s $destination/vim ~/.vim
 ln -s $destination/profile ~/.profile
 ln -s $destination/bashrc ~/.bashrc
+ln -s $destination/tmux.conf ~/.tmux.conf
 
 git submodule update --init --recursive
