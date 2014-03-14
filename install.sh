9da43c3e0748a77f9255c1fdb8c0a255cf6c012b
Noe Snaterse <noe@wiredpea.com>
2014-03-14 10:35:54 +0100

    Added my gitconfig to the install script too.

--- install.sh
+++ install.sh
@@ -12,11 +12,13 @@ rm -rf ~/.vim
 rm ~/.bashrc
 rm ~/.profile
 rm ~/.tmux.conf
+rm ~/.gitconfig
 
 ln -s $destination/vimrc ~/.vimrc
 ln -s $destination/vim ~/.vim
 ln -s $destination/profile ~/.profile
 ln -s $destination/bashrc ~/.bashrc
 ln -s $destination/tmux.conf ~/.tmux.conf
+ln -s $destination/gitconfig ~/.gitconfig
 
 git submodule update --init --recursive
